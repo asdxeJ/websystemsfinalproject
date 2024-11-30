26318e831868de3133f7c573706a4298ec0774bd
joe <joedaulayan@aldersgate.edu.ph>
2024-11-30 18:20:01 +0800

    rebuild

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7a163658509fda28738bbf28176795222112c18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfcdef8eea03c17199030b6d3c556d95f246f834")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
